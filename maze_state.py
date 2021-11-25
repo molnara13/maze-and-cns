87ece75c76cfe79fc104b1041d3f2b001bbdc517
molnara13 <36562477+molnara13@users.noreply.github.com>
2021-11-25 16:08:59 +0100

    Update maze_state.py
    Bugfix: add env parameter to create_image_view

--- maze_state.py
+++ maze_state.py
@@ -8,12 +8,12 @@ class State:
     def __init__(self, env):
 
             self._agent_view_by_cell = self._init_view(env)
-            self.create_image_view()
+            self.create_image_view(env)
 
     def _init_view(self, env):
             pass
 
-    def create_image_view(self):
+    def create_image_view(self, env):
             state_array = self._agent_view_by_cell
             image_base = np.uint8(env.colormap(image_base)*255)
             im = Image.fromarray(image_base)
