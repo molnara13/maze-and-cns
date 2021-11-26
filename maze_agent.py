70b0f59aee7836cb255a373152cb4888b99d0ab1
molnara13 <36562477+molnara13@users.noreply.github.com>
2021-11-26 01:40:21 +0100

    Update maze_agent.py
    Add method: process_image_input

--- maze_agent.py
+++ maze_agent.py
@@ -1,9 +1,13 @@
 from maze_environment import MazeEnvironment as env
 from agent import Agent
+from torchvision import transforms
 
 class MazeAgent(Agent):
 
     def __init__(self, self_aware=None, number_of_averaged=None):
             super().__init__(self_aware, number_of_averaged)
 
-    
+    def process_image_input(self, im):
+            transform = transforms.Compose([transforms.ToTensor()])
+            image_as_tensor = transform(im)
+            return image_as_tensor
